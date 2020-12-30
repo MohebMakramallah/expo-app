b46a83de27c49af5575d4a011a80d609f6eb8874
Moheb Makramallah <makramallah.moheb@thewebops.com>
2020-12-30 11:36:52 +0200

    running ios devices

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hi george!</Text>
+      <Text>hi george!</Text>
     </View>
   );
 }
@@ -12,7 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
